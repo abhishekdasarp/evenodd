9c157743039c6077a6d8a01b60b206ae54be9e44
abhishekdasarp <139537140+abhishekdasarp@users.noreply.github.com>
2023-07-30 20:04:24 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+class evenodd {
+    pulic
+    static void main(string[] args)
+    {
+        int no=12345;
+        if(no%==0)
+        {
+            system.out.println("number is even");
+        }
+            else
+            {
+                system.out.println("number is odd");
+            }
+            }
+}
